470c3ed58670887e5b5b81c1292b5206d72ca451
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-03-02 00:02:47 +0100

    calculate numbers with methods

---
+++ Task-1/index.js
@@ -0,0 +1,34 @@
+const calc = (initValue) => {
+  let result = initValue;
+
+  const calculator = {
+    add(number) {
+      result += number;
+      return this;
+    },
+
+    subtract(number) {
+      result -= number;
+      return this;
+    },
+
+    mult(number) {
+      result *= number;
+      return this;
+    },
+
+    div(number) {
+      result /= number;
+      return this;
+    },
+    result() {
+      return result;
+    },
+  };
+
+  return calculator;
+};
+
+const result = calc(3).add(5).subtract(7).mult(2).div(5).result();
+
+console.log(result);
